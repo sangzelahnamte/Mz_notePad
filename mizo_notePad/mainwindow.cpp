7c13d02fc176830c1d06c64074071e12cb31e84c
sangzelahnamte <sangaguitarist@gmail.com>
2025-05-15 20:58:40 -0400

    added further changes to functionality

--- mizo_notePad/mainwindow.cpp
+++ mizo_notePad/mainwindow.cpp
@@ -7,6 +7,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     init_widget();
+    setup_connections();
 }
 
 MainWindow::~MainWindow()
@@ -17,8 +18,42 @@ MainWindow::~MainWindow()
 void MainWindow::init_widget()
 {
     this->setCentralWidget(ui->textEdit);
+    // Tool Box
     font_box = new QFontComboBox(); // added to ui->toolBar parent
     fontSize_SB = new QSpinBox();
     ui->toolBar->addWidget(font_box);
     ui->toolBar->addWidget(fontSize_SB);
+    ui->toolBar->addSeparator();
+    ui->toolBar->addAction(ui->actionColour);
+    color_dialog = new QColorDialog(this);
 }
+
+void MainWindow::on_actionColour_triggered()
+{
+    color_dialog->exec();
+}
+
+void MainWindow::setup_connections()
+{
+    // Undo Redo
+    connect(ui->actionUndo, &QAction::triggered, ui->textEdit, &QTextEdit::undo);
+    connect(ui->actionRedo, &QAction::triggered, ui->textEdit, &QTextEdit::redo);
+    // Cut copy paste select all
+    connect(ui->actionCut, &QAction::triggered, ui->textEdit, &QTextEdit::cut);
+    connect(ui->actionCopy, &QAction::triggered, ui->textEdit, &QTextEdit::copy);
+    connect(ui->actionPaste, &QAction::triggered, ui->textEdit, &QTextEdit::paste);
+    connect(ui->actionSelect_All, &QAction::triggered, ui->textEdit, &QTextEdit::selectAll);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- mizo_notePad/mainwindow.h
+++ mizo_notePad/mainwindow.h
@@ -4,6 +4,7 @@
 #include <QMainWindow>
 #include <QFontComboBox>
 #include <QSpinBox>
+#include <QColorDialog>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -19,11 +20,16 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionColour_triggered();
+
 private:
     Ui::MainWindow *ui;
     QFontComboBox *font_box;
     QSpinBox *fontSize_SB; // font size spin box
+    QColorDialog *color_dialog;
 
     void init_widget();
+    void setup_connections();
 };
 #endif // MAINWINDOW_H
